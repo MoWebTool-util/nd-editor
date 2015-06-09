3f9d6330560c02c3926df8ce55acae4324ab42af
crossjs <liwenfu@crossjs.com>
2015-06-09 14:42:48 +0800

    update

--- src/plugins.js
+++ src/plugins.js
@@ -1,10 +1,6 @@
 'use strict';
 
 module.exports = [
-  // {
-  //   name: 'keys',
-  //   starter: require('./plugins/keys')
-  // },
   {
     name: 'paste',
     starter: require('./plugins/paste')
